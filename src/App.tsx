7b650afe082fb96c9355de72458aec2491637a83
Dhananjay-JSR <ajaysenday@gmail.com>
2024-05-06 01:37:44 +0530

    refactor:Made Job Card Modular

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import {
-  Box,
-  Button,
-  FormControl,
-  IconButton,
-  InputLabel,
-  MenuItem,
-  Select,
-  Stack,
-  SxProps,
-  TextField,
-  Theme,
-  ThemeProvider,
-  Typography,
-  createTheme,
-} from "@mui/material";
+import { Typography } from "@mui/material";
 import SideBar from "./Components/Sidebar";
 import MainStyle from "./App.module.css";
 import TabsMod from "./Components/Tabs";
-import SingleSelect from "./Components/Input/SingleSelect";
-import MultiSelect from "./Components/Input/MultiSelect";
 import Filters from "./Components/Filters";
-import {
-  ElectricBoltOutlined,
-  ElectricBoltTwoTone,
-  ExpandCircleDown,
-  ExpandCircleDownOutlined,
-  ThunderstormOutlined,
-} from "@mui/icons-material";
-import React from "react";
-import { lightGreen, blueGrey, pink } from "@mui/material/colors";
+import JOB_Cards from "./Components/JobCard";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 function App() {
-  const [isExpanded, setIsExpanded] = React.useState(false);
   return (
     <>
       <main className={MainStyle.main_wrapper}>
@@ -38,10 +13,16 @@ function App() {
         <section
           style={{
             width: "100%",
+            height: "100vh",
+            overflowY: "auto",
           }}
         >
           <header
             style={{
+              position: "sticky",
+              top: 0,
+              zIndex: 100,
+              backgroundColor: "white",
               padding: "10px",
               boxShadow: "0 2px 4px 0 rgba(0,0,0,0.1)",
               height: "50px",
@@ -54,165 +35,49 @@ function App() {
           <main className={MainStyle.body_holder}>
             <TabsMod />
             <Filters />
-            <Stack
-              gap={1.3}
-              style={{
-                width: "350px",
-                backgroundColor: "white",
-                borderRadius: "20px",
-                paddingTop: "20px",
-                paddingBottom: "20px",
-                paddingLeft: "30px",
-                paddingRight: "30px",
-                boxShadow: "#00000040 0px 1px 4px 0px",
+            <Grid2
+              justifyContent="center"
+              gap={2}
+              sx={{
+                mt: 2,
               }}
+              container
+              spacing={2}
             >
-              <Box
-                sx={{
-                  width: "fit-content",
-                  boxShadow: "#0606060d 0px 2px 6px 0px",
-                  padding: "4px 6px",
-                  borderRadius: "10px",
-                  border: "1px solid #e6e6e6",
-                }}
-              >
-                <Typography fontSize={"9px"}>⏳ Posted 5 days ago</Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  gap: "10px",
-                }}
-              >
-                <img
-                  style={{
-                    width: "25px",
-                    height: "2.5rem",
-                  }}
-                  src="https://logo.clearbit.com/dropbox.com"
-                />
-                <div>
-                  <Typography
-                    color={"#8b8b8b"}
-                    fontSize={"13px"}
-                    fontWeight={600}
-                    letterSpacing={"1px"}
-                    variant="h3"
-                    gutterBottom
-                  >
-                    Ema
-                  </Typography>
-                  <Typography
-                    variant="h2"
-                    fontWeight={400}
-                    fontSize={"14px"}
-                    lineHeight={"1.5"}
-                    gutterBottom
-                  >
-                    Software Productivity Engineer
-                  </Typography>
-                  <Typography
-                    variant="h2"
-                    fontWeight={500}
-                    fontSize={"11px"}
-                    lineHeight={"1.5"}
-                  >
-                    Bangalore
-                  </Typography>
-                </div>
-              </Box>
+              {API_Data.jdList.map((items) => {
+                return (
+                  <>
+                    <JOB_Cards>
+                      {(isExpanded, setExpanded) => (
+                        <>
+                          <JOB_Cards.Header
+                            salaryCurrencyCode={items.salaryCurrencyCode}
+                            minJdSalary={items.minJdSalary}
+                            maxJdSalary={items.maxJdSalary}
+                            location={items.location}
+                            logoUrl={items.logoUrl}
+                            jobRole={items.jobRole}
+                            companyName={items.companyName}
+                          />
+                          <JOB_Cards.Body
+                            isExpanded={isExpanded}
+                            setIsExpanded={setExpanded}
+                            jobDescription={items.jobDetailsFromCompany}
+                          />
+                          <JOB_Cards.Footer
+                            jdLink={items.jdLink}
+                            maxExp={items.maxExp}
+                            minExp={items.minExp}
+                          />
+                        </>
+                      )}
+                    </JOB_Cards>
+                  </>
+                );
+              })}
+            </Grid2>
 
-              <Typography color={"#4d596a"}>
-                Estimated Salary: ₹30 - 50LPA✅
-              </Typography>
-              <Box>
-                <Typography fontWeight={500}>Job Description:</Typography>
-                <Typography
-                  sx={{
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    display: "-webkit-box",
-                    WebkitLineClamp: isExpanded ? 10000 : 8,
-                    WebkitBoxOrient: "vertical",
-                    position: "relative",
-                    transition: "all 0.5s",
-                  }}
-                >
-                  <IconButton
-                    onClick={() => setIsExpanded(!isExpanded)}
-                    sx={{
-                      position: "absolute",
-                      bottom: 0,
-                      right: "50%",
-                      transform: `translateX(50%) `,
-                      zIndex: 1,
-                    }}
-                    color="primary"
-                    aria-label="Expand"
-                  >
-                    <ExpandCircleDownOutlined
-                      sx={{
-                        rotate: isExpanded ? "180deg" : "0deg",
-                      }}
-                    />
-                  </IconButton>
-
-                  {API_Data.jdList[0].jobDetailsFromCompany}
-                  {!isExpanded && (
-                    <Box
-                      sx={{
-                        backgroundImage:
-                          "linear-gradient(to bottom, rgba(255,255,255,0), rgba(255,255,255, 1) 90%)",
-                        width: "100%",
-                        position: "absolute",
-                        bottom: 0,
-                        left: 0,
-                        height: "70px",
-                      }}
-                    ></Box>
-                  )}
-                </Typography>
-              </Box>
-              <Box>
-                <Typography color={"#8b8b8b"} fontWeight={500}>
-                  Minimum Experience
-                </Typography>
-                <Typography>8 years</Typography>
-              </Box>
-              <Stack gap={1}>
-                <CustomBtn color="#54efc3">
-                  <ElectricBoltTwoTone
-                    sx={{
-                      color: "#f9c216",
-                    }}
-                  />
-                  Easy Apply
-                </CustomBtn>
-                <CustomBtn color="#4943da">
-                  <Stack direction={"row"} gap={1}>
-                    <img
-                      src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                      style={{
-                        borderRadius: "500%",
-                        filter: "blur(1px)",
-                        width: "20px",
-                        height: "20px",
-                      }}
-                    />
-                    <img
-                      src="https://i.pravatar.cc/150?u=a04saddsadsa"
-                      style={{
-                        borderRadius: "500%",
-                        filter: "blur(1px)",
-                        width: "20px",
-                        height: "20px",
-                      }}
-                    />
-                    Unlock referral asks
-                  </Stack>
-                </CustomBtn>
-              </Stack>
-            </Stack>
+            {/*  */}
           </main>
         </section>
       </main>
@@ -220,112 +85,8 @@ function App() {
   );
 }
 
-const CustomBtn = ({
-  children,
-  color,
-  sx,
-}: {
-  children: React.ReactNode;
-  color: string;
-  sx?: SxProps<Theme>;
-}) => {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: color,
-      },
-    },
-  });
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Button sx={sx} variant="contained">
-          {children}
-        </Button>
-      </ThemeProvider>
-    </>
-  );
-};
-
-const InfoViewer = ({
-  data,
-}: {
-  data: {
-    title: string;
-    children: {
-      title: string;
-      description: string;
-    }[];
-  }[];
-}) => {
-  return (
-    <Box
-      sx={{
-        height: "240px",
-        backgroundColor: "",
-        overflowY: "hidden",
-        position: "relative",
-      }}
-    >
-      <Box
-        sx={{
-          backgroundImage:
-            "linear-gradient(to bottom, rgba(255,255,255,0), rgba(255,255,255, 1) 90%)",
-          width: "100%",
-          position: "absolute",
-          bottom: 0,
-          left: 0,
-          height: "50px",
-        }}
-      ></Box>
-      {data.map((item) => {
-        return (
-          <Box>
-            <Typography fontWeight={500}>{item.title}</Typography>
-            {item.children.map((child) => {
-              return (
-                <Box>
-                  <Typography fontWeight={600}>{child.title}</Typography>
-                  <Typography>{child.description}</Typography>
-                </Box>
-              );
-            })}
-          </Box>
-        );
-      })}
-    </Box>
-  );
-};
-
-const AboutTag = [
-  {
-    title: "About Company:",
-    children: [
-      {
-        title: "About US",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam minus quam eum numquam dolor ducimus eligendi iusto quos minima officia non, aut totam, beatae saepe at aspernatur quis. Quia, minima.",
-      },
-      {
-        title: "Founder/Recruiter profiles",
-        description: "Suchit Dubey",
-      },
-    ],
-  },
-  {
-    title: "About Role:",
-    children: [
-      {
-        title: "Overview",
-        description:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, vitae distinctio dolorem voluptatem suscipit exercitationem iste porro! Aut voluptate quisquam, eius dolor assumenda fuga ullam molestiae nemo, architecto facilis sequi!",
-      },
-    ],
-  },
-];
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const API_Data = {
+export const API_Data = {
   jdList: [
     {
       jdUid: "cfff35ac-053c-11ef-83d3-06301d0a7178-92010",
---
+++ src/Components/JobCard/index.tsx
@@ -0,0 +1,271 @@
+import { Theme, ThemeProvider } from "@emotion/react";
+import {
+  ExpandCircleDownOutlined,
+  ElectricBoltTwoTone,
+} from "@mui/icons-material";
+import {
+  Stack,
+  Box,
+  Typography,
+  IconButton,
+  SxProps,
+  createTheme,
+  Button,
+} from "@mui/material";
+import React, { useEffect } from "react";
+import JobCardStyles from "./style.module.css";
+
+export default function JOB_Cards({
+  children,
+}: {
+  children: (
+    isExpanded: boolean,
+    setExpanded: React.Dispatch<React.SetStateAction<boolean>>
+  ) => React.ReactNode;
+}) {
+  const [isExpanded, setIsExpanded] = React.useState(false);
+
+  const Ref = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const HandleClickOutside = (e: MouseEvent) => {
+      if (Ref.current && !Ref.current.contains(e.target as Node)) {
+        setIsExpanded(false);
+      }
+    };
+    document.addEventListener("mousedown", HandleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", HandleClickOutside);
+    };
+  }, []);
+  return (
+    <Stack ref={Ref} gap={1.3} className={JobCardStyles.wrapper}>
+      {children(isExpanded, setIsExpanded)}
+    </Stack>
+  );
+}
+
+function BodyRenderer({
+  isExpanded,
+  setIsExpanded,
+  jobDescription,
+}: {
+  isExpanded: boolean;
+  setIsExpanded: React.Dispatch<React.SetStateAction<boolean>>;
+  jobDescription: string | null;
+}) {
+  return (
+    <>
+      <Box>
+        <Typography fontWeight={500}>Job Description:</Typography>
+        <Typography
+          sx={{
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            display: "-webkit-box",
+            WebkitLineClamp: isExpanded ? 10000 : 8,
+            WebkitBoxOrient: "vertical",
+            position: "relative",
+            transition: "all 0.5s",
+          }}
+        >
+          <IconButton
+            onClick={() => setIsExpanded(!isExpanded)}
+            sx={{
+              position: "absolute",
+              bottom: 0,
+              right: "50%",
+              transform: `translateX(50%) `,
+              zIndex: 1,
+            }}
+            color="primary"
+            aria-label="Expand"
+          >
+            <ExpandCircleDownOutlined
+              sx={{
+                rotate: isExpanded ? "180deg" : "0deg",
+              }}
+            />
+          </IconButton>
+
+          {jobDescription}
+          {!isExpanded && (
+            <Box className={JobCardStyles.body_description_fader}></Box>
+          )}
+        </Typography>
+      </Box>
+    </>
+  );
+}
+
+function BottomRenderer({
+  jdLink,
+  maxExp,
+  minExp,
+}: {
+  minExp: number | null;
+  maxExp: number | null;
+  jdLink: string;
+}) {
+  return (
+    <>
+      <Box
+        mt={"auto"}
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        {minExp != null && (
+          <Box>
+            <Typography color={"#8b8b8b"} fontWeight={500}>
+              Min Experience
+            </Typography>
+            <Typography>{minExp} Years</Typography>
+          </Box>
+        )}
+        {maxExp != null && (
+          <Box>
+            <Typography color={"#8b8b8b"} fontWeight={500}>
+              Max Experience
+            </Typography>
+            <Typography>{maxExp} years</Typography>
+          </Box>
+        )}
+      </Box>
+      <Stack gap={1}>
+        <CustomBtn
+          onClick={() => {
+            window.open(jdLink, "_blank");
+          }}
+          color="#54efc3"
+        >
+          <ElectricBoltTwoTone
+            sx={{
+              color: "#f9c216",
+            }}
+          />
+          Easy Apply
+        </CustomBtn>
+        <CustomBtn color="#4943da">
+          <Stack direction={"row"} gap={1}>
+            <img
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              className={JobCardStyles.blur_img}
+            />
+            <img
+              src="https://i.pravatar.cc/150?u=a04saddsadsa"
+              className={JobCardStyles.blur_img}
+            />
+            Unlock referral asks
+          </Stack>
+        </CustomBtn>
+      </Stack>
+    </>
+  );
+}
+
+function HeaderRenderer({
+  logoUrl,
+  companyName,
+  jobRole,
+  location,
+  maxJdSalary,
+  minJdSalary,
+  salaryCurrencyCode,
+}: {
+  logoUrl: string | null;
+  companyName: string | null;
+  jobRole: string | null;
+  location: string | null;
+  salaryCurrencyCode: string | null;
+  minJdSalary: number | null;
+  maxJdSalary: number | null;
+}) {
+  return (
+    <>
+      <Box className={JobCardStyles.Head_Wrapper}>
+        <Typography fontSize={"9px"}>⏳ Posted 5 days ago</Typography>
+      </Box>
+      <Box className={JobCardStyles.Head_wrap}>
+        {logoUrl != null && (
+          <img
+            style={{
+              width: "25px",
+              height: "2.5rem",
+            }}
+            src={logoUrl}
+          />
+        )}
+
+        <div>
+          <Typography
+            color={"#8b8b8b"}
+            fontSize={"13px"}
+            fontWeight={600}
+            letterSpacing={"1px"}
+            variant="h3"
+            gutterBottom
+          >
+            {companyName != null ? companyName : "Company Name"}
+          </Typography>
+          <Typography
+            variant="h2"
+            fontWeight={400}
+            fontSize={"14px"}
+            lineHeight={"1.5"}
+            gutterBottom
+          >
+            {jobRole != null ? jobRole : "Job Role"}
+          </Typography>
+          <Typography
+            variant="h2"
+            fontWeight={500}
+            fontSize={"11px"}
+            lineHeight={"1.5"}
+          >
+            {location != null ? location : "Location"}
+          </Typography>
+        </div>
+      </Box>
+      <Typography color={"#4d596a"}>
+        Estimated Salary: {minJdSalary != null ? "min: " + minJdSalary : ""}{" "}
+        {maxJdSalary != null ? "max: " + maxJdSalary : ""}
+        {salaryCurrencyCode != null ? " " + salaryCurrencyCode : ""} LPA✅
+      </Typography>
+    </>
+  );
+}
+
+function CustomBtn({
+  children,
+  color,
+  onClick,
+  sx,
+}: {
+  onClick?: () => void;
+  children: React.ReactNode;
+  color: string;
+  sx?: SxProps<Theme>;
+}) {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: color,
+      },
+    },
+  });
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Button onClick={onClick} sx={sx} variant="contained">
+          {children}
+        </Button>
+      </ThemeProvider>
+    </>
+  );
+}
+
+JOB_Cards.Header = HeaderRenderer;
+JOB_Cards.Body = BodyRenderer;
+JOB_Cards.Footer = BottomRenderer;
